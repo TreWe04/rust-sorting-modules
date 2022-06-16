2c837b0f24aa5e31d6dda355c9f5615a5ac9e21f
TreWe04 <TreWebb2016@gmail.com>
2022-06-15 22:14:41 -0400

    Added bubble sort and minor reformats

--- src/benchmarks/mod.rs
+++ src/benchmarks/mod.rs
@@ -1,3 +1,5 @@
+#![allow(while_true)]
+
 pub fn selection_sort (arr: &[i32]) -> Vec<i32> {
     if arr.len() == 0 {return vec![]};
     let mut arr1: Vec<i32> = arr.to_vec();
@@ -6,6 +8,7 @@ pub fn selection_sort (arr: &[i32]) -> Vec<i32> {
     for _ in 0..(arr1.len()-1) {
         let mut comparison = arr1[index];
         let mut min_index = index;
+
         for i in (index+1)..arr1.len() {
             if arr1[i] < comparison {
                 comparison = arr1[i];
@@ -16,7 +19,31 @@ pub fn selection_sort (arr: &[i32]) -> Vec<i32> {
         if min_index != index {
             arr1.swap(index, min_index);
         }
+
         index += 1;
+    }
+
+    arr1
+}
+
+
+pub fn bubble_sort (arr: &[i32]) -> Vec<i32> {
+    if arr.len() == 0 {return vec![]};
+    let mut arr1: Vec<i32> = arr.to_vec();
+
+    while true {
+        let mut has_swapped = false;
+
+        for i in 0..(arr1.len()-1){
+            if arr1[i] > arr1[i+1] {
+                arr1.swap(i, i+1);
+                has_swapped = true;
+            }
+        }
+
+        if !has_swapped {
+            break;
+        }
     }
 
     arr1
